46465a103f83495c315dab5c4ca2fd68728655ad
Poliduris1977 <42495919+Poliduris1977@users.noreply.github.com>
2026-02-19 11:19:10 +0300

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/neuromind/',
+  base: '/',
   plugins: [react()],
   resolve: {
     alias: {
